b929005aabdacf6d3e33df72d357d97827d6df26
rajiv tripathi <rajivtripathi.july10@gmail.com>
2020-04-19 16:02:04 +0530

    func updated

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,9 +1,9 @@
 def call(Map stageParams) 
-{
- 
-    checkout([
-        $class: 'GitSCM',
-        branches: [[name:  stageParams.branch ]],
-        userRemoteConfigs: [[ url: stageParams.url ]]
-    ])
+{   
+  checkout([  
+  $class: 'GitSCM', 
+  branches: [[name: stageParams.branch]],
+  userRemoteConfigs: [[url: stageParams.url]]  
+  ])
+  
   }
